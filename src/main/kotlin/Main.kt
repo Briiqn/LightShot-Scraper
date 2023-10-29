3ef00cb5485b2a91f7083d4cfc4d15ff43df31ab
Briiqn <82249811+Briiqn@users.noreply.github.com>
2023-10-29 19:06:12 -0400

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,16 +7,16 @@ import java.net.HttpURLConnection
 import java.net.URL
 import java.util.concurrent.Executors
 
-
+val charSet = charArrayOf(
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+    'r', 's', 't', 'u', 'x', 'w', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+)
 fun main(args : Array<String>) {
 println(generateLinks(100000))
 }
 
 fun generateLinks(count: Long): List<String> {
-    val charSet = charArrayOf(
-        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
-        'r', 's', 't', 'u', 'x', 'w', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
-    )
+
     val charCount = 6
         //limit to 2 to avoid ip ban
     val executor = Executors.newFixedThreadPool(2)
@@ -43,10 +43,7 @@ fun generateLinks(count: Long): List<String> {
 
 
 fun downloadImageFromUrl(imageUrl: String, savePath: String) {
-    val charSet = charArrayOf(
-        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
-        'r', 's', 't', 'u', 'x', 'w', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
-    )
+
     val randomChars = CharArray(16) { charSet.random() }
 
     val connection = URL(imageUrl).openConnection() as HttpURLConnection
